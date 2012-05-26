b38534c5aa5b0c03d898e095b7561efed808a5da
Guillaume BINET <gbin@gootz.net>
2012-05-26 14:18:41 +0200

    init commit

---
+++ README.rst
@@ -0,0 +1,20 @@
+err-time an err plugin to give the current time at a given place
+================================================================
+
+For more information about err you can find it here: https://github.com/gbin/err
+
+**Installation**
+
+*dependencies*
+It needs pytz:
+::
+    pip install pytz
+
+If you have the admin rights on an err chatbot simply use
+::
+
+    !install err-time
+
+in order to install the plugin.
+Then !help to see the available commands and their explanation.
+
---
+++ timeBot.py
@@ -0,0 +1,54 @@
+from botplugin import BotPlugin
+from jabberbot import botcmd
+import pytz
+
+tzs =  pytz.all_timezones_set
+fmt = '%Y-%m-%d %H:%M:%S %Z%z'
+
+def find_tz(city):
+    for x in pytz.all_timezones_set:
+        if city in x:
+            return x
+    return None
+
+def get_all_tznames():
+    country_timezones = {}
+    for (country, tzlist) in pytz.country_timezones.iteritems():
+        country_name = pytz.country_names[country]
+        cities = []
+        for timezone in tzlist:
+            city = re.sub(r'^[^/]*/', r'', timezone)
+            city = re.sub(country_name + '/', '', city)
+            city = re.sub(r'^([^/]*)/(.*)', r'\2, \1', city)
+            city = re.sub(r'_', r' ', city)
+            cities.append(city)
+            country_timezones[country_name] = cities
+    return country_timezones
+
+class TimeBot(BotPlugin):
+    @botcmd
+    def time(self, mess, args):
+        """ Shows the current time for given city.
+        Example: !time San Francisco
+        """
+        if not args:
+            return 'Am I supposed to guess the location?...'
+        city = args.strip().capitalize()
+        tz_name = find_tz(city)
+        if not tz_name:
+            return 'Sorry cannot find this city, you can list them with !tzlist'
+        tz = pytz.timezone(tz_name)
+        local_time = datetime.datetime.now(tz)
+        return 'Current time in %s : %s' % (tz_name, local_time.strftime(fmt))
+
+    @botcmd
+    def tzlist(self, mess, args):
+        """ List all the known cities
+        """
+        country_timezones = get_all_tznames()
+        answer = ''
+        for country in country_timezones:
+            answer+=country + ':\n'
+            for city in sorted(country_timezones[country]):
+                answer+='\t' + city + '\n'
+        return answer
