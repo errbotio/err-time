83e63ede2932fddfafa5e5d675dfbd28842abf35
Sijis Aviles <sijis.aviles@gmail.com>
2024-01-06 00:25:26 -0600

    refactor: simplify displaying all timezones

--- timeBot.py
+++ timeBot.py
@@ -1,11 +1,30 @@
 import datetime
-import re
 
 import pytz
 from errbot import BotPlugin, botcmd
 
-tzs =  pytz.all_timezones_set
 fmt = '%Y-%m-%d %H:%M:%S %Z%z'
+
+
+def find_tz(city):
+    for timezone in pytz.all_timezones_set:
+        if city in timezone:
+            return timezone
+    return None
+
+
+def get_all_tznames():
+    country_timezones = {}
+    for country, tzlist in pytz.country_timezones.items():
+        country_name = pytz.country_names[country]
+        cities = []
+        for timezone in tzlist:
+            raw_name = timezone.rpartition("/")[2]
+            name = raw_name.replace("_", " ")
+            cities.append(name)
+            country_timezones[country_name] = cities
+    return country_timezones
+
 
 def find_tz(city):
     for x in pytz.all_timezones_set:
@@ -13,19 +32,6 @@ def find_tz(city):
             return x
     return None
 
-def get_all_tznames():
-    country_timezones = {}
-    for (country, tzlist) in pytz.country_timezones.items():
-        country_name = pytz.country_names[country]
-        cities = []
-        for timezone in tzlist:
-            city = re.sub(r'^[^/]*/', r'', timezone)
-            city = re.sub(country_name + '/', '', city)
-            city = re.sub(r'^([^/]*)/(.*)', r'\2, \1', city)
-            city = re.sub(r'_', r' ', city)
-            cities.append(city)
-            country_timezones[country_name] = cities
-    return country_timezones
 
 class TimeBot(BotPlugin):
     @botcmd(split_args_with = ' ')
@@ -48,12 +54,11 @@ class TimeBot(BotPlugin):
 
     @botcmd
     def tzlist(self, mess, args):
-        """ List all the known cities
-        """
+        """List all the known cities"""
         country_timezones = get_all_tznames()
-        answer = ''
+        answer = ""
         for country in sorted(country_timezones):
-            answer+=country + ':\n'
+            answer += country + ":\n"
             for city in sorted(country_timezones[country]):
-                answer+='\t' + city + '\n'
+                answer += "\t" + city + "\n"
         return answer
