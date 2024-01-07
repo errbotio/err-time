4b660ea712dc77bb2029eb42515ed1969c28eaac
Sijis Aviles <sijis.aviles@gmail.com>
2024-01-07 00:19:35 -0600

    chore: format file

--- timeBot.py
+++ timeBot.py
@@ -3,7 +3,7 @@ import datetime
 import pytz
 from errbot import BotPlugin, botcmd
 
-fmt = '%Y-%m-%d %H:%M:%S %Z%z'
+FMT = "%Y-%m-%d %H:%M:%S %Z%z"
 
 
 def find_tz(city):
@@ -26,31 +26,27 @@ def get_all_tznames():
     return country_timezones
 
 
-def find_tz(city):
-    for x in pytz.all_timezones_set:
-        if city in x:
-            return x
-    return None
-
-
 class TimeBot(BotPlugin):
-    @botcmd(split_args_with = ' ')
-    def time(self, mess, args):
-        """ Shows the current time for given city.
+    @botcmd(split_args_with=" ")
+    def time(self, msg, args):
+        """Shows the current time for given city.
         Example: !time San Francisco
         """
         if not args:
-            return 'Am I supposed to guess the location?...'
-        if len(args) == 1 and args[0].lower() == 'utc':
-            tz_name = 'UTC'
+            return "Am I supposed to guess the location?..."
+
+        if len(args) == 1 and args[0].lower() == "utc":
+            tz_name = "UTC"
         else:
-            city = '_'.join([word.capitalize() for word in args])
+            city = "_".join([word.capitalize() for word in args])
             tz_name = find_tz(city)
+
         if not tz_name:
-            return 'Sorry cannot find this city, you can list them with !tzlist'
+            return "Sorry cannot find this city, you can list them with !tzlist"
+
         tz = pytz.timezone(tz_name)
         local_time = datetime.datetime.now(tz)
-        return 'Current time in %s : %s' % (tz_name, local_time.strftime(fmt))
+        return f"Current time in {tz_name}: {local_time.strftime(FMT)}"
 
     @botcmd
     def tzlist(self, mess, args):
