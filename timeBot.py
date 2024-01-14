39e9823110906c7f03741435539bc907e1f183f4
Sijis Aviles <sijis.aviles@gmail.com>
2024-01-13 21:43:40 -0600

    chore: various updates (#5)
    * fix: imports
    
    * refactor: simplify displaying all timezones
    
    * chore: format file
    
    * test: refactor to work with latest design
    
    * chore: update plugin minimum version
    
    * tests: add tox.ini
    
    * chore: add .gitignore
    
    * chore: add python tests github action

--- timeBot.py
+++ timeBot.py
@@ -1,65 +1,60 @@
-import datetime, pytz
-import re
+import datetime
 
-# Backward compatibility
-from errbot.version import VERSION
-from errbot.utils import version2array
-if version2array(VERSION) >= [1,6,0]:
-    from errbot import botcmd, BotPlugin
-else:
-    from errbot.botplugin import BotPlugin
-    from errbot.jabberbot import botcmd
+import pytz
+from errbot import BotPlugin, botcmd
 
-tzs =  pytz.all_timezones_set
-fmt = '%Y-%m-%d %H:%M:%S %Z%z'
+FMT = "%Y-%m-%d %H:%M:%S %Z%z"
+
 
 def find_tz(city):
-    for x in pytz.all_timezones_set:
-        if city in x:
-            return x
+    for timezone in pytz.all_timezones_set:
+        if city in timezone:
+            return timezone
     return None
+
 
 def get_all_tznames():
     country_timezones = {}
-    for (country, tzlist) in pytz.country_timezones.items():
+    for country, tzlist in pytz.country_timezones.items():
         country_name = pytz.country_names[country]
         cities = []
         for timezone in tzlist:
-            city = re.sub(r'^[^/]*/', r'', timezone)
-            city = re.sub(country_name + '/', '', city)
-            city = re.sub(r'^([^/]*)/(.*)', r'\2, \1', city)
-            city = re.sub(r'_', r' ', city)
-            cities.append(city)
+            raw_name = timezone.rpartition("/")[2]
+            name = raw_name.replace("_", " ")
+            cities.append(name)
             country_timezones[country_name] = cities
     return country_timezones
 
+
 class TimeBot(BotPlugin):
-    @botcmd(split_args_with = ' ')
-    def time(self, mess, args):
-        """ Shows the current time for given city.
+    @botcmd(split_args_with=" ")
+    def time(self, msg, args):
+        """Shows the current time for given city.
         Example: !time San Francisco
         """
         if not args:
-            return 'Am I supposed to guess the location?...'
-        if len(args) == 1 and args[0].lower() == 'utc':
-            tz_name = 'UTC'
+            return "Am I supposed to guess the location?..."
+
+        if len(args) == 1 and args[0].lower() == "utc":
+            tz_name = "UTC"
         else:
-            city = '_'.join([word.capitalize() for word in args])
+            city = "_".join([word.capitalize() for word in args])
             tz_name = find_tz(city)
+
         if not tz_name:
-            return 'Sorry cannot find this city, you can list them with !tzlist'
+            return "Sorry cannot find this city, you can list them with !tzlist"
+
         tz = pytz.timezone(tz_name)
         local_time = datetime.datetime.now(tz)
-        return 'Current time in %s : %s' % (tz_name, local_time.strftime(fmt))
+        return f"Current time in {tz_name}: {local_time.strftime(FMT)}"
 
     @botcmd
     def tzlist(self, mess, args):
-        """ List all the known cities
-        """
+        """List all the known cities"""
         country_timezones = get_all_tznames()
-        answer = ''
+        answer = ""
         for country in sorted(country_timezones):
-            answer+=country + ':\n'
+            answer += country + ":\n"
             for city in sorted(country_timezones[country]):
-                answer+='\t' + city + '\n'
+                answer += "\t" + city + "\n"
         return answer
